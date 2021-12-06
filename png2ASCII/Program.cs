39af789fcc9eb1f43ed21075ae0e680f7142a1c1
XmasApple <y4blokof@yandex.ru>
2021-12-06 06:10:50 +0300

    It's work

--- png2ASCII/Program.cs
+++ png2ASCII/Program.cs
@@ -1,12 +1,75 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 
 namespace png2ASCII
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        public static string Alphabet = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'.";
+
+        public const int Width = 240;
+        public const int Height = 60;
+        
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.SetWindowSize(Width, Height);
+            var img = Image.FromFile("test.jpg");
+            var resized = ResizeImage(img, Width, Height);
+            resized.Save("out1.png", ImageFormat.Png);
+            var gray = GrayScaleFilter(resized);
+            gray.Save("out2.png", ImageFormat.Png);
+            
+            for (var i = 0; i < Height; i++)
+            for (var j = 0; j < Width; j++)
+            {
+                var index = gray.GetPixel(j, i).R * (Alphabet.Length-1) / 255;
+                Console.Write(Alphabet[index]);
+            }
+
+            Console.ReadLine();
+        }
+        
+        public static Bitmap GrayScaleFilter(Bitmap image)
+        {
+            var grayScale = new Bitmap(image.Width, image.Height);
+
+            for (var y = 0; y < grayScale.Height; y++)
+            for (var x = 0; x < grayScale.Width; x++)
+            {
+                var c = image.GetPixel(x, y);
+
+                var gs = (int)(c.R * 0.3 + c.G * 0.59 + c.B * 0.11);
+
+                grayScale.SetPixel(x, y, Color.FromArgb(gs, gs, gs));
+            }
+            return grayScale;
+        }
+        
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width,image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
         }
     }
 }
