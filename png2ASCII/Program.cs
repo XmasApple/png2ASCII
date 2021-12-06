e06475399545cdc3aef05a0e60ad6bb2146f6f1a
XmasApple <y4blokof@yandex.ru>
2021-12-06 05:24:58 +0300

    Initial commit

---
+++ png2ASCII/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace png2ASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
